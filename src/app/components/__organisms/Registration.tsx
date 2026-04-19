d4ba446a1ef7fed500b133725d2f4fc24ca9e4ae
Giorgi Kostava <gkostava03@gmail.com>
2026-04-20 03:49:56 +0400

    Fix: route error

--- src/app/components/__organisms/Registration.tsx
+++ src/app/components/__organisms/Registration.tsx
@@ -118,7 +118,7 @@ export default function RegisterForm() {
         createdAt: new Date(),
       });
 
-      router.push("/home");
+      router.push("/");
     } catch (error: any) {
       alert(error.message);
     }
